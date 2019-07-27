7c52388cd0bd82199c2d9c1aad33fde16275dc36
assassin <assassin_cike@163.com>
2019-07-28 01:13:47 +0800

    test instance function of promise

--- promise/test/index.test.js
+++ promise/test/index.test.js
@@ -1,7 +1,21 @@
-import { sum } from "..";
+import { sum } from '..';
 
-describe("test promise", () => {
-  test("adds 1 + 2 to equal 3", () => {
+describe('test promise', () => {
+  test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
+
+  test('instance function', () => {
+    const instance = new Promise((resolve, reject) => {
+      resolve('success');
+    });
+    const mockFn = jest.fn();
+    instance.then(mockFn);
+
+    expect(mockFn).toBeCalled();
+    // instance.catch;
+    // instance.finally;
+  });
+
+  test('static function', () => {});
 });
