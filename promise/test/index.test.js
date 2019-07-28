36f71eeb269bf62ba025dd2667625fa107c4305b
assassin <assassin_cike@163.com>
2019-07-28 12:53:19 +0800

    add static function of promise

--- promise/test/index.test.js
+++ promise/test/index.test.js
@@ -5,17 +5,96 @@ describe('test promise', () => {
     expect(sum(1, 2)).toBe(3);
   });
 
-  test('instance function', () => {
+  test('instance function', done => {
+    expect.assertions(2);
     const instance = new Promise((resolve, reject) => {
-      resolve('success');
+      setTimeout(() => {
+        resolve('success');
+        done();
+        // Async function not behaving as I expect with Jest
+        // https://stackoverflow.com/questions/47275706/async-function-not-behaving-as-i-expect-with-jest
+      });
     });
-    const mockFn = jest.fn();
-    instance.then(mockFn);
 
-    expect(mockFn).toBeCalled();
+    // const mockFn = jest.fn();
+    instance.then(
+      res => {
+        expect(res).toBe('success');
+      },
+      err => {}
+    );
+
+    const rejectInstance = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject('fail');
+        done();
+        // Async function not behaving as I expect with Jest
+        // https://stackoverflow.com/questions/47275706/async-function-not-behaving-as-i-expect-with-jest
+      });
+    });
+
+    rejectInstance.then(
+      () => {},
+      err => {
+        expect(err).toBe('fail');
+      }
+    );
+
     // instance.catch;
     // instance.finally;
   });
 
-  test('static function', () => {});
+  test('catch with no reject function', done => {
+    expect.assertions(1);
+    const instance = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject('fail');
+        done();
+      });
+    });
+
+    instance.catch(exp => {
+      expect(exp).toBe('fail');
+    });
+  });
+
+  test('catch with reject function', done => {
+    expect.assertions(1);
+    const instance = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject('fail');
+        done();
+      });
+    });
+
+    instance
+      .then(
+        () => {},
+        err => {
+          expect(err).toBe('fail');
+        }
+      )
+      .catch(exp => {
+        expect(exp).toBe('fail'); // TODO:这样无法判断是走了reject函数还是走了catch函数
+      });
+  });
+
+  test('static function', () => {
+    expect.assertions(2);
+    const resolveInstance = Promise.resolve('geek');
+    resolveInstance.then(res => {
+      expect(res).toBe('geek');
+    });
+    const rejectInstance = Promise.reject('error');
+    rejectInstance.then(
+      () => {},
+      err => {
+        expect(err).toBe('error');
+      }
+    );
+    // Promise.reject
+    // Promise.all
+    // Promise.race
+    // Promise.once
+  });
 });
